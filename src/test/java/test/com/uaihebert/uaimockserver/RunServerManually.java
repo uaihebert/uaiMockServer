3912316af083e09c5ecccc804cc2a525b423f916
uaiHebert <hebolive@gmail.com>
2015-02-23 00:45:48 -0300

    adding all attributes to the request dialog; adding accordion; header and query param

--- src/test/java/test/com/uaihebert/uaimockserver/RunServerManually.java
+++ src/test/java/test/com/uaihebert/uaimockserver/RunServerManually.java
@@ -20,7 +20,7 @@ import com.uaihebert.uaimockserver.server.UaiMockServer;
 public class RunServerManually {
 
     public static void main(String[] args) throws Exception {
-        UaiMockServer.start();
+        UaiMockServer.start("configForListAllRoutes.config");
 
         Thread.currentThread().join();
     }
