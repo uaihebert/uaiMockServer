bde455525a2e3fd45c9a734debeff17712f12962
uaiHebert <hebolive@gmail.com>
2015-02-26 18:29:41 -0300

    creating 'add new' button to header/queryParam

--- src/main/java/com/uaihebert/uaimockserver/dto/factory/UaiRouteDTOFactory.java
+++ src/main/java/com/uaihebert/uaimockserver/dto/factory/UaiRouteDTOFactory.java
@@ -29,12 +29,13 @@ public final class UaiRouteDTOFactory {
         return uaiRouteDTOList;
     }
 
-    // todo create a back up of the file, before editing it. create a configuration for it
-    // todo handle error cases when GUI invoking the uaiMockServer
-    // todo there is a bug when editing the HTML array of the required header/query list. if no one if found, there is no way of adding it
     // todo when creating a new route and a content type was not send by the view, we should use the default
     // todo when creating a new route display the default content type as watermark
     // todo when including a new route, the delete button is available
+    // todo add placeholder in the inputs
+    // todo validate header/queryParam values when typed (name != null && headerValueList.length > 0)
+    // todo create delete button for and header/queryParam
+    // todo create a backup of the file, before editing it. create a configuration for it
     private static UaiRouteDTO createUaiRouteDTO(final UaiRoute uaiRoute) {
         final UaiFileDTO uaiFileDTO = UaiFileDTOFactory.create(uaiRoute.uaiFile);
         final UaiRequestDTO uaiRequestDTO = UaiRequestDTOFactory.create(uaiRoute.uaiRequest);
