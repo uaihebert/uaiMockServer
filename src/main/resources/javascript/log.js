4b77197acb2378c0e22737d700482a8d24a7f0cb
uaihebert <uaihebert@gmail.com>
2015-07-15 14:55:00 -0300

    fixing websocket url connection bug

--- src/main/resources/javascript/log.js
+++ src/main/resources/javascript/log.js
@@ -10,9 +10,25 @@ app.controller('webSocketController', function($scope, $location) {
     $scope.webSocketLog = {};
     $scope.webSocketLog.logRequestList = [];
     $scope.connectToWebSocket = function() {
+        function getWsURL() {
+            var loc = window.location, new_uri;
+
+            if (loc.protocol === "https:") {
+                new_uri = "wss:";
+            } else {
+                new_uri = "ws:";
+            }
+
+            new_uri += "//" + loc.host;
+            new_uri += loc.pathname + "-ws";
+            return  new_uri.replace('/page', '');
+        }
+
         if (window.WebSocket) {
-            // todo fix here. when is running in another server, this is not working
-            $scope.socket = new WebSocket("ws://localhost:1234/uaiGui-ws");
+            var url = getWsURL();
+
+            $scope.socket = new WebSocket(url);
+
             $scope.socket.onmessage = function (event) {
                 $scope.$apply(function() {
                     var jsonObj = JSON.parse(event.data);
